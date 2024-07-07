56bc83a521b8087295af16ac3c06b7e619921b3c
Abdulbaki Suraj <codelikesuraj@gmail.com>
2024-07-07 20:49:21 +0100

    update: more test

--- main_test.go
+++ main_test.go
@@ -21,12 +21,30 @@ import (
 	"gorm.io/gorm"
 )
 
+type RegisterSuccessResponse struct {
+	Data struct {
+		AccessToken string `json:"accessToken"`
+		User        struct {
+			Email     string `json:"email"`
+			FirstName string `json:"firstName"`
+			LastName  string `json:"lastName"`
+			Phone     string `json:"phone"`
+			UserID    string `json:"userId"`
+		} `json:"user"`
+	} `json:"data"`
+	Message string `json:"message"`
+	Status  string `json:"status"`
+}
+
 const (
 	letterBytes = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
 	numberBytes = "0123456789"
 )
 
-var db *gorm.DB
+var (
+	db     *gorm.DB
+	router *gin.Engine
+)
 
 func RandStringBytes(n int) string {
 	b := make([]byte, n)
@@ -61,6 +79,8 @@ func init() {
 		log.Fatal("error connecting to database:", err)
 	}
 	db.AutoMigrate(&models.Organisation{}, models.User{})
+
+	router = setupRouter()
 }
 
 func setupRouter() *gin.Engine {
@@ -86,8 +106,6 @@ func setupRouter() *gin.Engine {
 }
 
 func TestHomeRoute(t *testing.T) {
-	router := setupRouter()
-
 	w := httptest.NewRecorder()
 	req, _ := http.NewRequest("GET", "/", nil)
 	router.ServeHTTP(w, req)
@@ -97,8 +115,6 @@ func TestHomeRoute(t *testing.T) {
 }
 
 func TestAuthRoutes(t *testing.T) {
-	router := setupRouter()
-
 	t.Run("test user cannot register on validation error", func(t *testing.T) {
 		registerParamsJSON, _ := json.Marshal(map[string]string{})
 		w := httptest.NewRecorder()
@@ -201,5 +217,78 @@ func TestAuthRoutes(t *testing.T) {
 		req.Header.Set("Content-Type", "application/json")
 		router.ServeHTTP(w, req)
 		assert.Equal(t, http.StatusOK, w.Code, w.Body.String())
+	})
+}
+
+func TestAPIRoutes(t *testing.T) {
+	var resp RegisterSuccessResponse
+
+	registerParamsJSON, _ := json.Marshal(map[string]string{
+		"firstName": GenerateRandomString(10),
+		"lastName":  GenerateRandomString(10),
+		"email":     GenerateRandomEmail(),
+		"phone":     GenerateRandomNumber(),
+		"password":  GenerateRandomString(8),
+	})
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest("POST", "/auth/register", bytes.NewBuffer(registerParamsJSON))
+	req.Header.Set("Content-Type", "application/json")
+	router.ServeHTTP(w, req)
+	require.Equal(t, http.StatusCreated, w.Code, w.Body.String())
+
+	err := json.NewDecoder(w.Body).Decode(&resp)
+	require.Nil(t, err, err)
+
+	testToken := resp.Data.AccessToken
+	// testUser := resp.Data.User
+
+	t.Run("test unauthenticated user cannot access routes", func(t *testing.T) {
+		randNum := GenerateRandomNumber()
+		testCases := []struct {
+			method, url string
+		}{
+			{"GET", "/api/users/" + randNum},
+			{"GET", "/api/organisations/" + randNum},
+			{"GET", "/api/organisations"},
+			{"POST", "/api/organisations"},
+			{"POST", "/api/organisations/" + randNum + "/users"},
+		}
+
+		for _, testCase := range testCases {
+			w := httptest.NewRecorder()
+			req, _ := http.NewRequest(testCase.method, testCase.url, nil)
+			router.ServeHTTP(w, req)
+			assert.NotEqual(t, http.StatusInternalServerError, w.Code, resp)
+			assert.Equal(t, http.StatusUnauthorized, w.Code, fmt.Sprintln(w.Body.String(), ":", testCase.url))
+		}
+	})
+
+	t.Run("test authenticate user can access routes", func(t *testing.T) {
+		emptyParams, _ := json.Marshal(map[string]string{})
+		emptyParamsJson := bytes.NewBuffer(emptyParams)
+
+		randNum := GenerateRandomNumber()
+
+		testCases := []struct {
+			method, url string
+			body        *bytes.Buffer
+		}{
+			{"GET", "/api/users/" + randNum, emptyParamsJson},
+			{"GET", "/api/organisations/" + randNum, emptyParamsJson},
+			{"GET", "/api/organisations", emptyParamsJson},
+			{"POST", "/api/organisations", emptyParamsJson},
+			// {"POST", "/api/organisations/" + "1" + "/users", emptyParamsJson},
+		}
+
+		for _, testCase := range testCases {
+			w := httptest.NewRecorder()
+			req, _ := http.NewRequest(testCase.method, testCase.url, emptyParamsJson)
+			req.Header.Set("Authorization", "Bearer "+testToken)
+			req.Header.Set("Content-Type", "application/json")
+			router.ServeHTTP(w, req)
+			resp := fmt.Sprintln(w.Body.String(), ":", testCase.url)
+			assert.NotEqual(t, http.StatusInternalServerError, w.Code, resp)
+			assert.Equal(t, http.StatusUnauthorized, w.Code, resp)
+		}
 	})
 }
